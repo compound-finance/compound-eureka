dc429b543823200e0f8db755dc3c5d73558a9e7b
Geoffrey Hayes <hayesgm@gmail.com>
2020-09-20 11:57:16 -0700

    Use our own deployment of Uni

--- config/underlying.js
+++ config/underlying.js
@@ -332,19 +332,104 @@ define('Erc20', {
   }
 });
 
-define('Erc20', {
-  contract: 'Uni',
+// Build UNI Token
+define('Uni', {
   match: {
-    properties: {
-      'uni': true
+    default: true
+  },
+  contract: 'Uni',
+  properties: {
+    name: 'string',
+    decimals: 'number',
+    symbol: 'string',
+    balances: {
+      deferred: true,
+      dictionary: {
+        key: 'ref',
+        value: 'number'
+      },
+      setter: async (actor, contract, balances) => {
+        let {bn, deref, encode, ethereum, read, trx} = actor;
+        return Object.entries(balances).reduce(async (acc_, [ref, balance_]) => {
+          let acc = await acc_; // force ordering
+
+          let balance = bn(balance_);
+          let currentBalance = await balanceOf(actor, contract, ref);
+
+          if (currentBalance.gte(balance)) {
+            console.log(`Skipping ${ref} as account holds sufficient balance`);
+          } else {
+            let amount = balance.sub(currentBalance);
+
+            await trx(contract, 'transfer', [ref, amount]);
+
+            // Let's make sure the balance is, in fact, updated correctly
+            let newBalance = await balanceOf(actor, contract, ref);
+            if (newBalance.lt(balance)) {
+              throw new Error(`Expected balance for ${ref} to be greater than or equal to ${balance.toString()} but was ${newBalance.toString()}`);
+            }
+          }
+        }, Promise.resolve(null));
+      }
+    },
+    delegates: {
+      deferred: true,
+      dictionary: {
+        key: 'ref',
+        value: 'ref'
+      },
+      setter: async (actor, comp, delegates) => {
+        let {bn, deref, encode, ethereum, read, trx} = actor;
+        return Object.entries(delegates).reduce(async (acc_, [ref, delegate]) => {
+          let acc = await acc_; // force ordering
+
+          console.log(`Setting delegate for ${JSON.stringify(ref)} to ${JSON.stringify(delegate)}`);
+          if (deref(ref).address !== ethereum.from) {
+            throw new Error(`Cannot set delegate for ${JSON.stringify(ref)}`);
+          }
+
+          await trx(comp, 'delegate', [delegate]);
+        }, Promise.resolve(null));
+      }
     }
   },
+  build: async (actor, contract, {balances, delegates}, { definition }) => {
+    let {deploy, ethereum} = actor;
+    let zeroAddress = '0x0000000000000000000000000000000000000000';
+
+    let deployed = await deploy(contract, {
+      account: ethereum.from,
+      minter_: zeroAddress,
+      mintingAllowedAfter_: 9999999999999
+    });
+    if (balances) {
+      console.log(`Setting token balances for ${contract}...`);
+      await definition.typeProperties.balances.setter(actor, deployed, balances);
+    }
+    if (delegates) {
+      console.log(`Setting token delegates for ${contract}...`);
+      await definition.typeProperties.delegates.setter(actor, deployed, delegates);
+    }
+    return deployed;
+  }
+});
+
+define('Uni', {
+  match: {
+    has_properties: ['address']
+  },
+  contract: 'Uni',
   properties: {
-    uni: 'bool',
     address: 'address',
+    block: {
+      type: 'number',
+      setter: async ({trx}, contract, block) => {}
+    },
     name: 'string',
     decimals: 'number',
     symbol: 'string'
   },
-  build: async ({existing}, contract, {address}) => existing(contract, address)
+  build: async ({existing}, contract, { address }) => {
+    return existing(contract, address);
+  }
 });
